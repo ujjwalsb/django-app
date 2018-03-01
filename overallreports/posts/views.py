05b6a965a4a8fee6baa39fdd7810b5cd7e60b97e
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-01 16:49:12 +0530

    working on dynamic URL and patterns

--- overallreports/posts/views.py
+++ overallreports/posts/views.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Post
+
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context)
 
 def post_list(request):
 	queryset = Post.objects.all()
@@ -13,16 +21,11 @@ def post_list(request):
 		"object_list":queryset,
 		"title":"List"
 	}
-
 	return render(request, "index.html", context)
 	# return HttpResponse("<h1>List</h1")
 
-
 def post_create(request):
 	return HttpResponse("<h1>Create</h1")
-
-def post_detail(request):
-	return HttpResponse("<h1>Detail</h1")
 
 def post_update(request):
 	return HttpResponse("<h1>Update</h1")
