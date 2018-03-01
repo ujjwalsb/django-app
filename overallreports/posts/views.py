ba635628fd03bd1c8608fc306bce353ca204513d
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-01 12:54:20 +0530

    Added the urls for multiple pages and separated admin and post urls

--- overallreports/posts/views.py
+++ overallreports/posts/views.py
@@ -6,5 +6,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def post_home(request):
-	return HttpResponse("<h1>Hello</h1")
+def post_list(request):
+	return HttpResponse("<h1>List</h1")
+
+def post_create(request):
+	return HttpResponse("<h1>Create</h1")
+
+def post_detail(request):
+	return HttpResponse("<h1>Detail</h1")
+
+def post_update(request):
+	return HttpResponse("<h1>Update</h1")
+
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1")
