58fcc66b8f1711aa4d63792c8e1c9c6f990697cf
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-01 11:44:39 +0530

    Added admin properties, created first view and linked with the urls

--- overallreports/posts/views.py
+++ overallreports/posts/views.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def post_home(request):
+	return HttpResponse("<h1>Hello</h1")
