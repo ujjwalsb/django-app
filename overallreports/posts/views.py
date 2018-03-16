a4b7026d9d90bbf9f2a152c7b8bf52ba7056fcdc
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-16 15:13:27 +0530

    Created model form and view

--- overallreports/posts/views.py
+++ overallreports/posts/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
+from .forms import PostForm
 from django.http import HttpResponse
 from .models import Post
 
@@ -25,7 +26,15 @@ def post_list(request):
 	# return HttpResponse("<h1>List</h1")
 
 def post_create(request):
-	return HttpResponse("<h1>Create</h1")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 
 def post_update(request):
 	return HttpResponse("<h1>Update</h1")
