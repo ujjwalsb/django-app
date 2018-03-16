f8597de45d6abb08308788300cc19270a981a912
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-16 12:44:52 +0530

    Dynamic url with more better way

--- overallreports/overallreports/urls.py
+++ overallreports/overallreports/urls.py
@@ -20,5 +20,5 @@ from posts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls")),
+    url(r'^posts/', include("posts.urls", namespace='posts')),
 ]
