5af96bc0d706988b9c2c06699636020b12f422ad
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-18 17:04:39 +0530

    setting up of static files

--- overallreports/overallreports/urls.py
+++ overallreports/overallreports/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from posts import views
@@ -22,3 +24,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls", namespace='posts')),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
