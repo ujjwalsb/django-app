92972de7ef412c528e0732371d8985bdcb4a8cb5
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-19 19:28:10 +0530

    Added slug fields and urls

--- overallreports/posts/models.py
+++ overallreports/posts/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
 from django.core.urlresolvers import reverse
 
 def upload_location(instance, filename):
@@ -11,6 +13,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
 	title = models.CharField(max_length=120)
+	slug = models.SlugField(unique=True)
 	image = models.ImageField(upload_to=upload_location,
 			null=True,
 			blank=True,
@@ -30,7 +33,25 @@ class Post(models.Model):
 		return self.title
 
 	def get_absolute_url(self):
-		return reverse("posts:detail", kwargs={"id":self.id})
+		return reverse("posts:detail", kwargs={"slug":self.slug})
 
 	class Meta:
 		ordering = ["-timestamp", "-updated"]
+
+# Code for converting the post url with title name rather than a number.
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)		# To convert url
+	if new_slug is not None:
+		slug = new_slug
+	qs = Post.objects.filter(slug=slug).order_by("-id")
+	exists = qs.exists()
+	if exists:
+		new_slug = "%s-%s" %(slug, qs.first().id)
+		return create_slug(instance, new_slug=new_slug)
+	return slug
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
